f6ad12d6966f0ac4025a6c3d85f9004178ff9853
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-30 14:52:44 +0300

    create: component -FormContact description: Полностью создал форму

--- src/components/FormContact.tsx
+++ src/components/FormContact.tsx
@@ -3,13 +3,25 @@ import React, {FC} from 'react';
 const FormContact:FC = () => {
     return (
         <div className='form-contact'>
-            <div className='container'>
+            <div className='container form-block'>
                 <div className="form-wrapper">
                     <h2 className="tittle-form">Вызвать специалиста для замера, консультации и составления расчета!</h2>
                     <form className="form-container">
-                        <input type="text"/>
-                        <input type="text"/>
-                        <button>Оставить заявку</button>
+                        <h3 className="title-form">Наш специалист ответит вам в течении 5 минут в рабочее время и приедет в удобное время. Мы составим расчет в течение 24 часов и свяжемся с вами.</h3>
+                        <input
+                            type="text"
+                            className='f-input'
+                            placeholder='+380-(999)-999-999'
+                        />
+                        <input
+                            type="text"
+                            className='f-input'
+                            placeholder='Ваше имя'
+                        />
+                        <button
+                            className='f-button'
+                            type='button'
+                        >Оставить заявку</button>
                     </form>
                 </div>
             </div>
