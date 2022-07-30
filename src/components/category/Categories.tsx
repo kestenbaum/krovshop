89672e484c2b44f4dc792a70faafbf4a07435e0f
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-30 15:41:23 +0300

    create: component -Categories description: создал отдельную компоненту для всех категорий

---
+++ src/components/category/Categories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Category from "./Category";
+
+const Categories = () => {
+    return (
+        <div className='categories'>
+            <Category/>
+            <Category/>
+            <Category/>
+        </div>
+    );
+};
+
+export default Categories;
