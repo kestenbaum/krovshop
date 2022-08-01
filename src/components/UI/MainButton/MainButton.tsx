fe75c1095bc52d14cca2a8c7b71c5d51f02898bf
Vovnenko Oleksii <kespa71for@gmail.com>
2022-08-01 18:54:53 +0300

    fix: component -MainButton description: отфиксил путь в кнопке

--- src/components/UI/MainButton/MainButton.tsx
+++ src/components/UI/MainButton/MainButton.tsx
@@ -2,13 +2,15 @@ import React, {FC} from 'react';
 import style from '../MainButton/MainButton.module.css'
 import {NavLink} from "react-router-dom";
 
-interface IMainButton extends React.HTMLProps<HTMLLinkElement>{}
+interface IMainButton extends React.HTMLProps<HTMLLinkElement>{
+    page: string
+}
 
-const MainButton:FC<IMainButton> = ({children}) => {
+const MainButton:FC<IMainButton> = ({children, page}) => {
 
     return (
         <NavLink
-            to='/page'
+            to={page}
             className={style.btn}
         >
             {children}
