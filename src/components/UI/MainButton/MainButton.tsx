9d371b8e931da86e7e0c00f688ea18a9e492750c
Vovnenko Oleksii <kespa71for@gmail.com>
2022-08-09 15:06:02 +0300

    fix: componet -mainBtn description: фикс главной кнопки

--- src/components/UI/MainButton/MainButton.tsx
+++ src/components/UI/MainButton/MainButton.tsx
@@ -1,13 +1,18 @@
 import React, {FC} from 'react';
 import style from '../MainButton/MainButton.module.css'
 
-interface IMainButton extends React.HTMLProps<HTMLLinkElement>{}
+interface IMainButton extends
+    React.DetailedHTMLProps<React.HTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+{}
 
-const MainButton:FC<IMainButton> = ({children}) => {
+const MainButton:FC<IMainButton> = (props) => {
+
+    const {children, ...rest} = props
 
     return (
         <button
             className={style.btn}
+            {...rest}
         >
             {children}
         </button>
