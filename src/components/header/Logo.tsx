8b928c283c819626ef68c2a5c2ebdcebc67644a5
Vovnenko Oleksii <kespa71for@gmail.com>
2022-08-23 14:36:44 +0300

    fix: style -Logo description: ***

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react';
 import logo from '../../assets/img/logo.svg'
+import {NavLink} from "react-router-dom";
 
 
 const Logo:FC = () => {
     return (
-        <div className='logo'>
+        <NavLink to='/' className='logo'>
             <img src={logo} alt='Логотип сайта'/>
-        </div>
+        </NavLink>
     );
 };
 
